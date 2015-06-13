b54ed73e97d0a89e75a2acdad967ca23e8c6cfe0
Stephen Guerra <raphael.appsoluteperfection@gmail.com>
2015-06-13 00:07:05 -0500

    Initial Commit
    Basic API out of the box created.

---
+++ GraphiteAlert/GraphiteAlert/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace GraphiteAlert.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ GraphiteAlert/GraphiteAlert/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GraphiteAlert.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}

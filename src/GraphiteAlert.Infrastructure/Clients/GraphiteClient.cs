a7c30af42a12d3ccd33793f8fc5ffa612b85fb91
Stephen Guerra <raphael.appsoluteperfection@gmail.com>
2015-06-13 21:38:43 -0500

    Basic API call for the first leaf

--- src/GraphiteAlert.Infrastructure/Clients/GraphiteClient.cs
+++ src/GraphiteAlert.Infrastructure/Clients/GraphiteClient.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
 using GraphiteAlert.Infrastructure.Clients.Dtos;
 using GraphiteAlert.Infrastructure.Configuration;
+using GraphiteAlert.Infrastructure.Extensions;
+using Newtonsoft.Json;
 
 namespace GraphiteAlert.Infrastructure.Clients
 {
@@ -15,12 +19,27 @@ namespace GraphiteAlert.Infrastructure.Clients
 
         public IEnumerable<GraphiteGraphDto> GetAll()
         {
-            throw new System.NotImplementedException();
+            var allFromRoot = GetAllFromRoot(); // TODO, cache
+            return allFromRoot;
         }
 
         public IEnumerable<GraphiteGraphDto> Get(string searchQuery)
         {
             throw new System.NotImplementedException();
+        }
+
+        private IEnumerable<GraphiteGraphDto> GetAllFromRoot()
+        {
+            return GetAllFrom("*");
+        }  
+        
+        private IEnumerable<GraphiteGraphDto> GetAllFrom(string q)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, _graphiteSettings.GetSearchUri(q));
+            var response = request.GetResponse();
+            var json = response.Content.ReadAsStringAsync().Result;
+            var results = JsonConvert.DeserializeObject<IEnumerable<GraphiteGraphDto>>(json);
+            return results;
         }
     }
 }
---
+++ src/GraphiteAlert.Infrastructure/Extensions/WebRequestExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+
+namespace GraphiteAlert.Infrastructure.Extensions
+{
+    public static class HttpRequestMessageExtensions
+    {
+        public static HttpResponseMessage GetResponse(this HttpRequestMessage request)
+        {
+            var handler = new HttpClientHandler
+            {
+                AllowAutoRedirect = false
+            };
+            var httpClient = new HttpClient(handler)
+            {
+                Timeout = TimeSpan.FromSeconds(30)
+            };
+            var sendAsync = httpClient.SendAsync(request);
+            return sendAsync.Result;
+        } 
+    }
+}

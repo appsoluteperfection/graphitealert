2e60e1d8d67b663e9d727b92b25f16b6fd7915e2
unknown <nsarlo@NSARLO-LT.local.uship.com>
2015-10-16 19:14:53 -0500

    data parsed ready for dto

--- src/GraphiteAlert.Infrastructure/Clients/IGraphiteClient.cs
+++ src/GraphiteAlert.Infrastructure/Clients/IGraphiteClient.cs
@@ -1,11 +1,13 @@
-﻿using GraphiteAlert.Infrastructure.Clients.Dtos;
+﻿using System;
+using GraphiteAlert.Infrastructure.Clients.Dtos;
 using System.Collections.Generic;
 
 namespace GraphiteAlert.Infrastructure.Clients
 {
-    interface IGraphiteClient
+    public interface IGraphiteClient
     {
         IEnumerable<GraphiteGraphDto> GetAll();
         IEnumerable<GraphiteGraphDto> Get(string searchQuery);
+        IEnumerable<Tuple<dynamic, dynamic>> GetDataPoints(string collection);
     }
 }
